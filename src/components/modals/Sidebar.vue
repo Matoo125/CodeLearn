317f784904fc0a7cf151e08a658d0e7982f5c821
Matej Vrzala <vrzala.matej@gmail.com>
2017-07-18 15:06:17 +0200

    html alfa finished

--- src/components/modals/Sidebar.vue
+++ src/components/modals/Sidebar.vue
@@ -1,15 +1,7 @@
 <template>
 <b-aside :is-show="isShown" :show-footer="false" title="Lessons" @close="isShown=false" placement="right">
-<!-- 
-      <li><a>1.2 Attributes</a></li>
-      <li><a>1.4 Links</a></li>
-      <li><a>1.5 Tables</a></li>
-      <li><a>1.6 Lists</a></li>
-      <li><a>1.7 Div & Span</a></li>
-      <li><a>1.8 Styles</a></li>
-      <li><a>1.9 Images</a></li>
-      <li><a>1.10 Sematic</a></li>
- -->
+
+
   <div v-for="topic in this.$store.state.code.learn">
     <p class="menu-label">
       {{ topic.id }}. {{ topic.title }}
